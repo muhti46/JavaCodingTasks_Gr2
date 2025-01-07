61bc7a151c6871c7186030ffde5ce7b7492c2180
muhti46 <muhteremdemir@gmail.com>
2025-01-07 18:55:25 +0100

    initial commit EvenOddTask is created

---
+++ src/week01/OddOrEven_muhterem.java
@@ -0,0 +1,17 @@
+package week01;
+
+public class OddOrEven_muhterem {
+    /*
+**Numbers -- odd & even**
+
+       Write a method that can identify  a given number is even or odd.
+
+       EX:
+
+              identify(5) --> "Odd"
+              identify(6) --> "Even"
+ */
+    public static void main(String[] args) {
+
+    }
+}
