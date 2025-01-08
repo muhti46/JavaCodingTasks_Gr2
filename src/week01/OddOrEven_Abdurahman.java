78c85ee848a867872bdd3bcd2d558c32e58dec53
Abdurahman512 <oabdurahman512@gmail.com>
2025-01-08 15:25:28 +0100

    OddOrEven Taks Finshed By Abdurahman

--- src/week01/OddOrEven_Abdurahman.java
+++ src/week01/OddOrEven_Abdurahman.java
@@ -1,5 +1,7 @@
 package week01;
 
+import java.util.Scanner;
+
 public class OddOrEven_Abdurahman {
 
     /*
@@ -13,6 +15,18 @@ public class OddOrEven_Abdurahman {
               identify(6) --> "Even"
  */
     public static void main(String[] args) {
-        System.out.println("OddOrEven Abdurahman");
+        // create scanner object
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int number = input.nextInt();
+        oddOrEven(number);
+
+    }
+    public static void oddOrEven(int number) {
+       if (number % 2 == 0) {
+           System.out.println("Even");
+       }else {
+           System.out.println("Odd");
+       }
     }
 }
