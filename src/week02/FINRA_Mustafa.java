81429bb091e858a5ca920fc2e4c435e5fb0f8204
mustafatemirak <mustafatemirak@hotmail.com>
2025-01-21 21:38:27 +0100

    FINRA Task, after review

--- src/week02/FINRA_Mustafa.java
+++ src/week02/FINRA_Mustafa.java
@@ -1,26 +1,36 @@
 package week02;
 
 public class FINRA_Mustafa {
-    /**
-     * Write a function that prints out the numbers from 1 to 30
-     * but for numbers that are a multiple of 3,
-     * print "FIN" instead of the number, and for numbers that are a multiple of 5,
-     * print "RA" instead of the number.
-     * For numbers that are a multiple of both 3 and 5, print "FINRA" instead of
-     * the number.
-     */
+
+    //Our main method (execution starts here)
     public static void main(String[] args) {
-        for (int i = 1; i <=30 ; i++) {
-            if (i % 3 == 0 && i % 5 == 0){
+
+     /*
+     We are calling to belows method to process numbers from 1 to 30 (if we want, we can change the numbers )
+     based on the "finraNumbers"'s rules
+     */
+
+        finraNumbers(1, 30);
+    }
+
+    public static void finraNumbers(int num1, int num2) {
+
+       //We use for loop to access all numbers
+        for (int i = num1; i <= num2; i++) {
+
+            //if the number is divisible by both 3 and 5, print "FINRA"
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FINRA ");
+            //if the numbers is divisible only 3, print "FIN"
             } else if (i % 3 == 0) {
                 System.out.print("FIN ");
+            //if the numbers is divisible only 5, print "RA"
             } else if (i % 5 == 0) {
                 System.out.print("RA ");
-            }else {
+            //if the number is not divisible 3 or 5, print the number!
+            } else {
                 System.out.print(i + " ");
             }
         }
     }
-
 }
