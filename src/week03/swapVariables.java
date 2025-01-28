80bc63b1b63ddc08edfb343a04d1aea2f03d9a7b
mecha <ahmet.kurt.2010@gmail.com>
2025-01-28 16:49:12 +0100

    swapTowVariables

---
+++ src/week03/swapVariables.java
@@ -0,0 +1,29 @@
+package week03;
+
+/*
+Swap two integer variable values
+(including both ways; with using a third variable and without it)
+ */
+public class swapVariables {
+    public static void main(String[] args) {
+
+        int var01 = 12;
+        int var02 = 1;
+        int temp;
+
+        System.out.println("Before swap: var01 = "+var01);
+        System.out.println("Before swap: var02 = "+var02);
+
+        temp=var01;
+        var01=var02;
+        var02=temp;
+
+
+        System.out.println("After swap: var01 = "+var01);
+        System.out.println("After swap: var02 = "+var02);
+
+
+
+    }
+
+}
